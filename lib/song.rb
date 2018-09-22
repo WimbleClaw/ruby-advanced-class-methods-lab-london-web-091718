d2c095f1176c448072ee015f778b32c6e5073067
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-22 23:11:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) = []
+    if self.find_by_name(name) == []
       self.create_by_name
     else self.find_by_name
   end
