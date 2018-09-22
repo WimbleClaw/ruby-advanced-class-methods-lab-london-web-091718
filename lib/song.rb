d59049e22cef0fe06e9833b90ca266229b81f4fc
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-22 22:39:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,8 @@ class Song
   end
 
   def self.new_by_name(name)
-    self.all.collect do |song| song.name == name
+    self.all.collect do |song| binding.pry
+      song.name == name
     end
   end
 end
